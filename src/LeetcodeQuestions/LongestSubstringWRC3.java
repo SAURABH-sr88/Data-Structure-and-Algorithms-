58f3f5d1c12a6679a556c409a4bfe8400c3fc956
SAURABH <saurabh.srivastava1024@gmail.com>
2025-09-17 16:19:33 +0530

    3

---
+++ src/LeetcodeQuestions/LongestSubstringWRC3.java
@@ -0,0 +1,29 @@
+package LeetcodeQuestions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWRC3 {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+
+        int maxLength = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+
+            // If character is already in the set, remove from left
+            while (set.contains(ch)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(ch);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+}
