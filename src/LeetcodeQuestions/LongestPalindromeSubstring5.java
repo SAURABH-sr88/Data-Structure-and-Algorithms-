0fba8cb300463ea0c88a5c152f3c287aa869bc63
SAURABH <saurabh.srivastava1024@gmail.com>
2025-09-17 19:24:43 +0530

    5

---
+++ src/LeetcodeQuestions/LongestPalindromeSubstring5.java
@@ -0,0 +1,28 @@
+package LeetcodeQuestions;
+
+public class LongestPalindromeSubstring5 {
+   public static String longPal(String s){
+       if (s == null || s.isEmpty()) return s;
+
+       int start = 0 , end = 0;
+       for (int i = 0; i < s.length(); i++) {
+           int len1 = expandAroundCentre(s, i, i);
+           int len2 = expandAroundCentre(s, i, i + 1);
+
+           int len = Math.max(len1, len2);
+
+           if (len > end - start ){
+               start  = i - (len - 1)/2;
+               end = i + len / 2;
+           }
+       }
+       return s.substring(start, end + 1);
+   }
+   private static int expandAroundCentre (String s, int left, int right){
+       while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+           left --;
+           right ++;
+       }
+       return right - left - 1;
+   }
+}
